23c07e1e485dee6c5dbf813d7b95f88b8f768fd7
雨巷 <43312224+chenyuxiang0425@users.noreply.github.com>
2020-06-23 09:08:51 +0800

    add scheme

---
+++ scheme/questions.scm
@@ -0,0 +1,39 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+
+;; Problem 15
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 15
+  'replace-this-line
+  )
+  ; END PROBLEM 15
+
+;; Problem 16
+
+;; Merge two lists LIST1 and LIST2 according to COMP and return
+;; the merged lists.
+(define (merge comp list1 list2)
+  ; BEGIN PROBLEM 16
+  'replace-this-line
+  )
+  ; END PROBLEM 16
+
+
+(merge < '(1 5 7 9) '(4 8 10))
+; expect (1 4 5 7 8 9 10)
+(merge > '(9 7 5 1) '(10 8 4 3))
+; expect (10 9 8 7 5 4 3 1)
+
+;; Problem 17
+
+(define (nondecreaselist s)
+    ; BEGIN PROBLEM 17
+    'replace-this-line
+    )
+    ; END PROBLEM 17
+
+
