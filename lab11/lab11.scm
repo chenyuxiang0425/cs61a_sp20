4d4a8868ecb0c88bca2466918f11086a744ef70c
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-06-24 13:05:06 +0800

    finish lab11.scm

--- lab11/lab11.scm
+++ lab11/lab11.scm
@@ -1,17 +1,35 @@
+; https://github.com/bahgaat/cs61a/blob/f406368dc6774607ebadf41bfa863f06cdb7f619/lab11/lab11_extra.scm
+; I don't understand...
 (define (flatmap f x)
-  (cond
-    (x)
-    (y)
-    (f)
-    (l)
+  (define (helper k f x)
+    (cond
+      ((null? x) k)
+      (else (helper (append k (f (car x)) ) f (cdr x)))
+    )
   )
-  )
+  (helper nil f x)
+)
 
 (define (expand lst)
-  'YOUR-CODE-HERE)
+  (define (g x)
+  (cond
+    ((equal? x 'x) '(x r y f r))
+    ((equal? x 'y) '(l f x l y))
+    (else (list x))
+  )
+)
+  (flatmap g lst)
+  )
 
 (define (interpret instr dist)
-  'YOUR-CODE-HERE)
+  (cond
+    ((null? instr) (fd 0))
+    ((equal? (car instr) 'f) (begin (fd dist)  (interpret (cdr instr) dist)))
+    ((equal? (car instr) 'l) (begin (lt 90) (interpret (cdr instr) dist)))
+    ((equal? (car instr) 'r) (begin (rt 90)  (interpret (cdr instr) dist)))
+    (else (interpret (cdr instr) dist))
+  )
+)
 
 (define (apply-many n f x)
   (if (zero? n)
