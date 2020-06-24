e375d8d1350978c3aacfcba5b0aca8fafe9dc041
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-06-24 14:16:43 +0800

    finish hw09

--- hw09/hw09.sql
+++ hw09/hw09.sql
@@ -26,16 +26,21 @@ CREATE TABLE sizes AS
 -- copy your solution from prev hw!
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.name as name,b.size as size from dogs as a, sizes as b where a.height > b.min and a.height <= b.max;
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_parent_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.child from parents as a, dogs as b where a.parent = b.name order by height DESC;
 
 -- Filling out this helper table is optional
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.child as first, b.child as second from parents as a, parents as b where a.parent = b.parent and a.child < b.child;
 
 -- Sentences about siblings that are the same size
+CREATE TABLE siblings_have_same_size AS
+  SELECT a.first as first, a.second as second, b.size as size
+  from siblings as a, size_of_dogs as b, size_of_dogs as c
+  where a.first = b.name and a.second = c.name and b.size = c.size;
+
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT first || ' and ' || second || ' are ' || size || ' siblings' from siblings_have_same_size;
