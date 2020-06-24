458fac3fc287419dc2011def6b966156c62fa157
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-06-24 15:21:08 +0800

    add lab12

---
+++ lab12/lab12.py
@@ -0,0 +1,341 @@
+from operator import add, sub, mul
+
+def prune_min(t):
+    """Prune the tree mutatively from the bottom up.
+
+    >>> t1 = Tree(6)
+    >>> prune_min(t1)
+    >>> t1
+    Tree(6)
+    >>> t2 = Tree(6, [Tree(3), Tree(4)])
+    >>> prune_min(t2)
+    >>> t2
+    Tree(6, [Tree(3)])
+    >>> t3 = Tree(6, [Tree(3, [Tree(1), Tree(2)]), Tree(5, [Tree(3), Tree(4)])])
+    >>> prune_min(t3)
+    >>> t3
+    Tree(6, [Tree(3, [Tree(1)])])
+    """
+    "*** YOUR CODE HERE ***"
+
+def remainders_generator(m):
+    """
+    Yields m generators. The ith yielded generator yields natural numbers whose
+    remainder is i when divided by m.
+
+    >>> import types
+    >>> [isinstance(gen, types.GeneratorType) for gen in remainders_generator(5)]
+    [True, True, True, True, True]
+    >>> remainders_four = remainders_generator(4)
+    >>> for i in range(4):
+    ...     print("First 3 natural numbers with remainder {0} when divided by 4:".format(i))
+    ...     gen = next(remainders_four)
+    ...     for _ in range(3):
+    ...         print(next(gen))
+    First 3 natural numbers with remainder 0 when divided by 4:
+    4
+    8
+    12
+    First 3 natural numbers with remainder 1 when divided by 4:
+    1
+    5
+    9
+    First 3 natural numbers with remainder 2 when divided by 4:
+    2
+    6
+    10
+    First 3 natural numbers with remainder 3 when divided by 4:
+    3
+    7
+    11
+    """
+    "*** YOUR CODE HERE ***"
+
+def foldr(link, fn, z):
+    """ Right fold
+    >>> lst = Link(3, Link(2, Link(1)))
+    >>> foldr(lst, sub, 0) # (3 - (2 - (1 - 0)))
+    2
+    >>> foldr(lst, add, 0) # (3 + (2 + (1 + 0)))
+    6
+    >>> foldr(lst, mul, 1) # (3 * (2 * (1 * 1)))
+    6
+    """
+    "*** YOUR CODE HERE ***"
+
+def mapl(lst, fn):
+    """ Maps FN on LST
+    >>> lst = Link(3, Link(2, Link(1)))
+    >>> mapl(lst, lambda x: x*x)
+    Link(9, Link(4, Link(1)))
+    """
+    "*** YOUR CODE HERE ***"
+
+# Account
+class Account(object):
+    """A bank account that allows deposits and withdrawals.
+
+    >>> eric_account = Account('Eric')
+    >>> eric_account.deposit(1000000)   # depositing my paycheck for the week
+    1000000
+    >>> eric_account.transactions
+    [('deposit', 1000000)]
+    >>> eric_account.withdraw(100)      # buying dinner
+    999900
+    >>> eric_account.transactions
+    [('deposit', 1000000), ('withdraw', 100)]
+    """
+
+    interest = 0.02
+
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount):
+        """Increase the account balance by amount and return the
+        new balance.
+        """
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        """Decrease the account balance by amount and return the
+        new balance.
+        """
+        if amount > self.balance:
+            return 'Insufficient funds'
+        self.balance = self.balance - amount
+        return self.balance
+
+class CheckingAccount(Account):
+    """A bank account that charges for withdrawals.
+
+    >>> check = Check("Steven", 42)  # 42 dollars, payable to Steven
+    >>> steven_account = CheckingAccount("Steven")
+    >>> eric_account = CheckingAccount("Eric")
+    >>> eric_account.deposit_check(check)  # trying to steal steven's money
+    The police have been notified.
+    >>> eric_account.balance
+    0
+    >>> check.deposited
+    False
+    >>> steven_account.balance
+    0
+    >>> steven_account.deposit_check(check)
+    42
+    >>> check.deposited
+    True
+    >>> steven_account.deposit_check(check)  # can't cash check twice
+    The police have been notified.
+    """
+    withdraw_fee = 1
+    interest = 0.01
+
+    def withdraw(self, amount):
+        return Account.withdraw(self, amount + self.withdraw_fee)
+
+    "*** YOUR CODE HERE ***"
+
+class Check(object):
+    "*** YOUR CODE HERE ***"
+
+
+def foldl(link, fn, z):
+    """ Left fold
+    >>> lst = Link(3, Link(2, Link(1)))
+    >>> foldl(lst, sub, 0) # (((0 - 3) - 2) - 1)
+    -6
+    >>> foldl(lst, add, 0) # (((0 + 3) + 2) + 1)
+    6
+    >>> foldl(lst, mul, 1) # (((1 * 3) * 2) * 1)
+    6
+    """
+    if link is Link.empty:
+        return z
+    "*** YOUR CODE HERE ***"
+    return foldl(______, ______, ______)
+
+def filterl(lst, pred):
+    """ Filters LST based on PRED
+    >>> lst = Link(4, Link(3, Link(2, Link(1))))
+    >>> filterl(lst, lambda x: x % 2 == 0)
+    Link(4, Link(2))
+    """
+    "*** YOUR CODE HERE ***"
+
+def reverse(lst):
+    """ Reverses LST with foldl
+    >>> reverse(Link(3, Link(2, Link(1))))
+    Link(1, Link(2, Link(3)))
+    >>> reverse(Link(1))
+    Link(1)
+    >>> reversed = reverse(Link.empty)
+    >>> reversed is Link.empty
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+identity = lambda x: x
+
+def foldl2(link, fn, z):
+    """ Write foldl using foldr
+    >>> list = Link(3, Link(2, Link(1)))
+    >>> foldl2(list, sub, 0) # (((0 - 3) - 2) - 1)
+    -6
+    >>> foldl2(list, add, 0) # (((0 + 3) + 2) + 1)
+    6
+    >>> foldl2(list, mul, 1) # (((1 * 3) * 2) * 1)
+    6
+    """
+    def step(x, g):
+        "*** YOUR CODE HERE ***"
+    return foldr(link, step, identity)(z)
+
+def num_splits(s, d):
+    """Return the number of ways in which s can be partitioned into two
+    sublists that have sums within d of each other.
+
+    >>> num_splits([1, 5, 4], 0)  # splits to [1, 4] and [5]
+    1
+    >>> num_splits([6, 1, 3], 1)  # no split possible
+    0
+    >>> num_splits([-2, 1, 3], 2) # [-2, 3], [1] and [-2, 1, 3], []
+    2
+    >>> num_splits([1, 4, 6, 8, 2, 9, 5], 3)
+    12
+    """
+    "*** YOUR CODE HERE ***"
+
+# Link Class
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+# Tree Class
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def map(self, fn):
+        """
+        Apply a function `fn` to each node in the tree and mutate the tree.
+
+        >>> t1 = Tree(1)
+        >>> t1.map(lambda x: x + 2)
+        >>> t1.map(lambda x : x * 4)
+        >>> t1.label
+        12
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> t2.map(lambda x: x * x)
+        >>> t2
+        Tree(9, [Tree(4, [Tree(25)]), Tree(16)])
+        """
+        self.label = fn(self.label)
+        for b in self.branches:
+            b.map(fn)
+
+    def __contains__(self, e):
+        """
+        Determine whether an element exists in the tree.
+
+        >>> t1 = Tree(1)
+        >>> 1 in t1
+        True
+        >>> 8 in t1
+        False
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> 6 in t2
+        False
+        >>> 5 in t2
+        True
+        """
+        if self.label == e:
+            return True
+        for b in self.branches:
+            if e in b:
+                return True
+        return False
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+# naturals
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
---
+++ lab12/lab12.scm
@@ -0,0 +1,5 @@
+; Lab 14: Final Review
+
+(define (compose-all funcs)
+  'YOUR-CODE-HERE
+)
